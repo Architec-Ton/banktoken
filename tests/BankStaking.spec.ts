73637f8c11a2390d327c9be7feed37b840465b7b
Stan Takt <st.takt@gmail.com>
2024-06-18 17:55:35 +0300

    added claim ARCs functionality, testing WIP, refactored  msgs and structs

--- tests/BankStaking.spec.ts
+++ tests/BankStaking.spec.ts
@@ -58,8 +58,29 @@ describe('BankStaking', () => {
             success: true,
         });
 
+        bankStaking = blockchain.openContract(await BankStaking.fromInit(alice.address, bankJetton.address));
+
+        const deployResultBS = await bankStaking.send(
+            deployer.getSender(),
+            {
+                value: toNano('10'),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            },
+        );
+
+        expect(deployResultBS.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: bankStaking.address,
+            deploy: true,
+            success: true,
+        });
         ARCJetton = blockchain.openContract(
-            await AJ.ArcJetton.fromInit(owner.address, buildOnchainMetadata(ARCjettonParams)),
+            await AJ.ArcJetton.fromInit(owner.address, 
+                bankStaking.address, 
+                buildOnchainMetadata(ARCjettonParams)),
         );
         const deployResult = await ARCJetton.send(
             owner.getSender(),
@@ -79,25 +100,6 @@ describe('BankStaking', () => {
             success: true,
         });
 
-        bankStaking = blockchain.openContract(await BankStaking.fromInit(alice.address, bankJetton.address, parameter));
-
-        const deployResultBS = await bankStaking.send(
-            deployer.getSender(),
-            {
-                value: toNano('10'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            },
-        );
-
-        expect(deployResultBS.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: bankStaking.address,
-            deploy: true,
-            success: true,
-        });
     });
     it('stake BNK', async () => {
         // Mint 1 token to Alice first to build her jetton wallet
@@ -162,16 +164,16 @@ describe('BankStaking', () => {
         // });
 
         const stakeStorage = blockchain.openContract(await StakeStorage.fromAddress(stakeStorageAddr));
-        const amountTime = await stakeStorage.getAmountTime();
+        const amountTime = await stakeStorage.getAmountTime(alice.address);
         expect(amountTime.stakedAmount).toEqual(1n);
-        
+
         //checking iterator (point to itself now)
         const pn =  await stakeStorage.getPrevnextcells() 
         const stakeStorageP = blockchain.openContract(await StakeStorage.fromAddress(pn.previous));
-        const amountTimeP = await stakeStorageP.getAmountTime();
+        const amountTimeP = await stakeStorageP.getAmountTime(alice.address);
         expect(amountTimeP.stakedAmount).toEqual(1n);
         const stakeStorageN = blockchain.openContract(await StakeStorage.fromAddress(pn.next));
-        const amountTimeN = await stakeStorageN.getAmountTime();
+        const amountTimeN = await stakeStorageN.getAmountTime(alice.address);
         expect(amountTimeN.stakedAmount).toEqual(1n);
 
     });
