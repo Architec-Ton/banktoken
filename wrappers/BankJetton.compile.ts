d3acc374fd5eba7d6f40b652c46a50711cc2a023
Stan Takt <st.takt@gmail.com>
2024-06-25 15:02:27 +0300

    fixing BNKUnstake WIP

--- wrappers/BankJetton.compile.ts
+++ wrappers/BankJetton.compile.ts
@@ -3,4 +3,7 @@ import { CompilerConfig } from '@ton/blueprint';
 export const compile: CompilerConfig = {
     lang: 'tact',
     target: 'contracts/bank_jetton.tact',
+    options: {
+        debug: true, // ← that's the stuff!
+      }
 };
